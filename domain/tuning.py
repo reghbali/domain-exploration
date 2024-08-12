7b60ca3c4dcfa8a395e4bf54e34d54d86c9db314
Reza <eghbali@uw.edu>
2024-08-12 10:56:59 -0700

    added dataset dataset info

--- domain/tuning.py
+++ domain/tuning.py
@@ -1,6 +1,6 @@
-import sys
-
 import argparse
+from collections import namedtuple
+import sys
 
 import torch.nn as nn
 import lightning.pytorch as pl
@@ -21,10 +21,21 @@ from domain.data.data_modules import (
 )
 from domain.model.lit_modules import LitClassificationModel as LiT
 
+DatasetInfo = namedtuple('DatasetInfo', ['name', 'image_shape', 'num_classes'])
+dataset_info_table = {
+    'MNIST': DatasetInfo('MNIST', (28, 28, 1), 10),
+    'CIFAR10': DatasetInfo('CIFAR10', (32, 32, 3), 10),
+    'ImageNet': DatasetInfo('ImageNet', (224, 224, 3), 1000),
+}
+
 valid_examples = 0.1
 
+
 def objective(
-    trial: optuna.trial.Trial, model: nn.Module, datamodule: pl.LightningDataModule, epochs: int
+    trial: optuna.trial.Trial,
+    model: nn.Module,
+    datamodule: pl.LightningDataModule,
+    epochs: int,
 ) -> float:
     model = LiT(model, datamodule.num_classes)
 
@@ -69,14 +80,14 @@ def get_args():
 
 
 def build_model(
-    trial: optuna.trial.Trial, architecture: str, domain: str, dataset: str
+    trial: optuna.trial.Trial, architecture: str, domain: str, dataset_info: DatasetInfo
 ) -> nn.Module:
     if architecture == 'mlp':
         hidden_factor = trial.suggest_int('hidden_factor', 1, 16)
         depth = trial.suggest_int('depth', 1, 5)
         model = MLP(
-            input_shape=(28, 28),
-            num_classes=10,
+            input_shape=dataset_info.image_shape,
+            num_classes=dataset_info.num_classes,
             hidden_factor=hidden_factor,
             depth=depth,
         )
@@ -86,7 +97,9 @@ def build_model(
             trial.suggest_int(f'layer_{i}', 32, 128)
             for i in range(trial.suggest_int('depth', 1, 5))
         )
-        model = ResidualMLP(input_shape=(28, 28, 1), layers=layers, num_classes=10)
+        model = ResidualMLP(
+            input_shape=dataset_info.image_shape, layers=layers, num_classes=10
+        )
 
     elif architecture == 'cnn':
         conv1_feature_maps = trial.suggest_int('conv1_feature_maps', 16, 64)
@@ -97,9 +110,9 @@ def build_model(
         linear_size2 = trial.suggest_int('linear_size2', 64, 256)
         linear_size3 = trial.suggest_int('linear_size3', 32, 128)
         model = CNN(
-            input_shape=(28, 28),
-            num_classes=10,
-            in_channels=1,
+            input_shape=dataset_info.image_shape[:-1],
+            num_classes=dataset_info.num_classes,
+            in_channels=dataset_info.image_shape[-1],
             conv1_feature_maps=conv1_feature_maps,
             conv2_feature_maps=conv2_feature_maps,
             kernel_size=kernel_size,
@@ -120,43 +133,34 @@ def build_model(
         model = ResCNN(ResCNN_block, layers=layers, in_channels=1, num_classes=10)
 
     elif architecture == 'vit':
-        if dataset == 'CIFAR10':
+        if dataset_info.name == 'CIFAR10':
             patch_size = trial.suggest_categorical('patch_size', [8])
             embed_dim = trial.suggest_categorical('embed_dim', [256])
             num_attention_heads = trial.suggest_categorical('num_attention_heads', [8])
             num_layers = trial.suggest_categorical('num_layers', [8])
             attention_head_dim = trial.suggest_categorical('attention_head_dim', [32])
             mlp_head_dim = trial.suggest_categorical('mlp_head_dim', [1024])
-            input_shape = (3, 32, 32)
-            in_channels = 3
-            num_classes = 10
 
-        elif dataset == 'MNIST':
+        elif dataset_info.name == 'MNIST':
             patch_size = trial.suggest_categorical('patch_size', [7])
             embed_dim = trial.suggest_categorical('embed_dim', [256])
             num_attention_heads = trial.suggest_categorical('num_attention_heads', [8])
             num_layers = trial.suggest_categorical('num_layers', [8])
             attention_head_dim = trial.suggest_categorical('attention_head_dim', [32])
             mlp_head_dim = trial.suggest_categorical('mlp_head_dim', [1024])
-            input_shape = (1, 28, 28)
-            in_channels = 1
-            num_classes = 10
 
-        elif dataset == 'ImageNet':
+        elif dataset_info.name == 'ImageNet':
             patch_size = trial.suggest_categorical('patch_size', [16])
             embed_dim = trial.suggest_categorical('embed_dim', [768])
             num_attention_heads = trial.suggest_categorical('num_attention_heads', [12])
             num_layers = trial.suggest_categorical('num_layers', [12])
             attention_head_dim = trial.suggest_categorical('attention_head_dim', [64])
             mlp_head_dim = trial.suggest_categorical('mlp_head_dim', [3072])
-            input_shape = (3, 224, 224)
-            in_channels = 3
-            num_classes = 1000
 
         model = VisionTransformer(
-            input_shape=input_shape,
-            in_channels=in_channels,
-            num_classes=num_classes,
+            input_shape=dataset_info.image_shape,
+            in_channels=dataset_info.image_shape[-1],
+            num_classes=dataset_info.num_classes,
             dropout_rate=0.1,
             patch_size=patch_size,
             embed_dim=embed_dim,
@@ -185,13 +189,17 @@ def build_datamodule(
 if __name__ == '__main__':
     args = get_args()
 
-    pruner = (optuna.pruners.MedianPruner() if args.pruning else optuna.pruners.NopPruner())
+    pruner = (
+        optuna.pruners.MedianPruner() if args.pruning else optuna.pruners.NopPruner()
+    )
 
     study = optuna.create_study(direction='maximize', pruner=pruner)
     study.optimize(
         lambda trial: objective(
             trial,
-            build_model(trial, args.architecture, args.domain, args.dataset),
+            build_model(
+                trial, args.architecture, args.domain, dataset_info_table[args.dataset]
+            ),
             build_datamodule(args.dataset, args.domain, args.batchsize),
             args.epochs,
         ),
